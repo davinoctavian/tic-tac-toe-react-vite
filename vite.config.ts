55f0d3afea40e0db8c9ecc954b07f6787f71a356
davinoctavian <davinoctavian@gmail.com>
2026-01-26 13:35:41 +0700

    change the image files

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     }),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["pwa-192.png"], // what is use on index.html is include here for service worker caching
+      includeAssets: ["pwa-192.PNG"], // what is use on index.html is include here for service worker caching
       manifest: {
         name: "TicTacToe",
         short_name: "TicTacToe",
@@ -21,8 +21,8 @@ export default defineConfig({
         display: "standalone",
         start_url: "/",
         icons: [
-          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/pwa-512.webp", sizes: "512x512", type: "image/webp" },
+          { src: "/pwa-192.PNG", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
         ],
       },
       workbox: {
